e17b14a4e51ba0efe72a0301edd136a44ebb771f
maryo <dennydelcidmario@gmail.com>
2025-05-19 14:10:39 +0200

    fix:curriculum mario

---
+++ skills/nose.js
@@ -0,0 +1,29 @@
+let num1 = 10;
+let num2 = 25;
+
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+console.log(Math.sqrt(num2))
+
+
+let frutas = ["manzana", "pera", "uva", "mango", "fresa"];
+console.log(frutas[1])
+console.log(frutas[3])
+console.log(frutas[0])
+
+
+let age = prompt("Cuantos años tienes?")
+age = parseInt(age)
+
+
+if (age >= 18){
+
+    console.log("eres mayor de edad, puedes ingressar")
+}
+else{
+    console.log("Eres menor de edad, no debes entrar, es ilegal apostar para ti")
+}
